b80668ac94c5f23c64cd425361d8770e10e0c18e
Xiaoming Lu <xiaoming@ufl.edu>
2017-08-29 16:44:40 -0400

    Assignment3 submit

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,15 +7,35 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config'),
+    fileData = undefined,
+    listingData =undefined;
 
 /* Connect to your database */
+mongoose.connect(config.db.uri);
+
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
+ fileData = fs.readFileSync('listings.json', 'utf8', function(err) {
+    if (err) {
+      throw err;
+    }
+});
 
+ listingData = JSON.parse(fileData).entries;
+
+ listingData.forEach(function(entry){
+  var newList = new Listing(entry);
+   newList.save(function(err){
+    if (err) {
+      throw err;
+    }
+    console.log("Creating new entry");
+  })
+ });
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- ListingSchema.js
+++ ListingSchema.js
@@ -5,12 +5,27 @@ var mongoose = require('mongoose'),
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here */
+  code: { type: String, required: true},
+  name: { type: String, required: true},
+  coordinates: {
+  	latitude: Number,
+  	longitude: Number
+  },
+  address: String
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
   /* your code here */
+  if(!this.created_at)
+  	this.created_at = new Date();
+  
+  	this.updated_at = new Date();
+
+  console.log("Save/Update new entry");
+  return next();
 });
+
 
 /* Use your schema to instantiate a Mongoose model */
 var Listing = mongoose.model('Listing', listingSchema);
